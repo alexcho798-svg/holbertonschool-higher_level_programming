35c03920af707143f255042567860c27f75cb67d
alexcho798-svg <alexcho798@gmail.com>
2026-03-05 09:02:00 -0600

    Add comments to user creation SQL script
    Added comments for clarity in user creation SQL script.

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,8 @@
-
+-- Crear el usuario si no existe
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
-
+-- Conceder todos los privilegios en el servidor
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
+-- Aplicar cambios
 FLUSH PRIVILEGES;
