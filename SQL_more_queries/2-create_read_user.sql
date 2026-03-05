b1dda71cd2af6873222bab87617813f4f33a74a5
alexcho798-svg <alexcho798@gmail.com>
2026-03-05 08:57:27 -0600

    Add comments to SQL script for user creation

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,11 +1,11 @@
-
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
-
+-- Create the user if it doesn't exist
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
-
+-- Grant only SELECT privilege on hbtn_0d_2 to user_0d_2
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
-
+-- Apply changes
 FLUSH PRIVILEGES;
